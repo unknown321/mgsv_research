c1695b0d7de7a1cce67fe586a26d16507787b878
unknown321 <unknown@unknown>
2016-09-19 13:59:58 +0300

    dropping current weapons at will

--- mods/good_subsistence/good_subsistence.lua
+++ mods/good_subsistence/good_subsistence.lua
@@ -1,5 +1,6 @@
 local good_subsistence = {}
 local helpers = require('helpers')
+local debug = false
 
 good_subsistence.state = 0
 
@@ -7,10 +8,21 @@ function switch_fulton()
 	if good_subsistence.state == 0 then
 		vars.playerDisableActionFlag = 	PlayerDisableAction.FULTON
 		good_subsistence.state = 1
-		helpers.log('[good_subsistence] Fulton disabled', true)
+		if debug then
+			helpers.log('[good_subsistence] Fulton disabled', true)
+		end
 	else
 		vars.playerDisableActionFlag = 	0
 		good_subsistence.state = 0
-		helpers.log('[good_subsistence] Fulton enabled', true)
+		if debug then
+			helpers.log('[good_subsistence] Fulton disabled', true)
+		end
+	end
+end
+
+function drop_weapon()
+	Player.UnsetEquip{ slotType = vars.currentInventorySlot, dropPrevEquip = true, }
+	if debug then
+		helpers.log('[good_subsistence] Dropped current weapon', true)
 	end
 end
